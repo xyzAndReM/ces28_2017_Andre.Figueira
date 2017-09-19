05990ea5340b85c85211b22a35282cd65b97f483
andre <a.marcello92@gmail.com>
2017-09-19 20:52:04 -0300

    malditos sinais de menos do inferno

--- Labs/lab02/src/lab02/CalculadoraString.java
+++ Labs/lab02/src/lab02/CalculadoraString.java
@@ -9,22 +9,31 @@ public class CalculadoraString {
 		String separator = new String();
 		Pattern p = Pattern.compile("\\[(.*?)\\]"); //Ler todos as strings dentro de  colchetes
 		Matcher m = p.matcher(numbers);
+		String rep = "[";
+		boolean flag = false;
 		String pattern = "(\\\\)?(\\[(.*?)\\])*(\n)?(\\s+)*((-?\\d+)?((\\s)*,(\\s)*)*((\\s)*(\n)(\\s)*)*"; //Patter default que considera
-		// como separador a , e o \n. Além disto, ignora todos os espaços entre os separadores.
+		// como separador a , e o \n. Alï¿½m disto, ignora todos os espaï¿½os entre os separadores.
 		while(m.find()) {
 			separator ="\\Q" +  m.group(1) + "\\E"; //O \Q e o \E servem para ler a expressao literalmente, evitando que elementos pre definidos
 			// no regex causem problemas, ex: *.
+			rep += "(" + separator + ")*";
+			flag = true;
 		    pattern += "((\\s)*" + separator + "(\\s)*)*"; // adicionando ao patter default o novo separador
-		    numbers2 = numbers2.replaceAll(separator, ","); // trocando todos os separadores novos na string pelo separador
-		    // default , . Isto ajuda no caso do símbolo -
+		     // trocando todos os separadores novos na string pelo separador
+		    // default , . Isto ajuda no caso do sï¿½mbolo -
 		}
 		pattern += ")*";
+		rep += "]+";
+		if(flag) {	
+			numbers2 = numbers2.replaceAll(rep, ",");
+		}
+		System.out.println(numbers2);
+		System.out.println(rep);
 		
 		Pattern verificador = Pattern.compile(pattern);
-		System.out.println(verificador);
 		Matcher aceitavel = verificador.matcher(numbers);
 		 if (!aceitavel.matches()) {
-		        throw new IllegalArgumentException(pattern + "\n" + numbers2);
+		        throw new IllegalArgumentException(pattern);
 		 }
 		
 		LinkedList<String> numbers_list = new LinkedList<String>();
--- Labs/lab02/src/lab02/CalculadoraStringComplexTest.java
+++ Labs/lab02/src/lab02/CalculadoraStringComplexTest.java
@@ -12,59 +12,93 @@ public class CalculadoraStringComplexTest {
 	@Test
 	public void MinusSeparator() {
 		String simples = new String();
-		simples = "\\[-][p]1-2-3";
+		simples = "\\[-][p]\n1-2-3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	
 	@Test
 	public void SpaceSeparator() {
 		String simples = new String();
-		simples = "\\[ ][p]1 2     3";
+		simples = "\\[ ][p]\n1 2     3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	
 	@Test
 	public void AsteriskSeparator() {
 		String simples = new String();
-		simples = "\\[*][p]1*2*3";
+		simples = "\\[*][p]\n1*2*3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	@Test
 	public void InvertedBarSeparator() {
 		String simples = new String();
-		simples = "\\[\\][p]1\\2\\3";
+		simples = "\\[\\][p]\n1\\2\\3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void LongSeparator() {
 		String simples = new String();
-		simples = "\\[aaa][p]1aa2aa3";
+		simples = "\\[aaa][p]\n1aa2aa3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	@Test
 	public void barDseparator() {
 		String simples = new String();
-		simples = "\\[\\D][p]1\\D2\\D3";
+		simples = "\\[\\D][p]\n1\\D2\\D3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	@Test
 	public void aspasSeparator() {
 		String simples = new String();
-		simples = "\\['][p]1'2'3";
+		simples = "\\['][p]\n1'2'3";
 		assertEquals(6,CalculadoraString.add(simples));
 	}
 	@Test
 	public void multipleAsterisksSeparator() {
 		String simples = new String();
-		simples = "\\[*][**][**][p]1*2**3***4";
+		simples = "\\[*][**][**][p]\n1*2**3***4";
 		assertEquals(10,CalculadoraString.add(simples));
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void multipleAsterisksSeparatorButOnlyOneSeparatorOnStringRaiseException() {
 		String simples = new String();
-		simples = "\\[**][p]1*2*3*4";
+		simples = "\\[**][p]1\n*2*3*4";
 		assertEquals(10,CalculadoraString.add(simples));
 	}
+	@Test(expected = IllegalArgumentException.class)
+	public void multipleAsterisksSeparatorButSeparatorisHigherYetNotMultiple() {
+		String simples = new String();
+		simples = "\\[**][p]1\n***2***3***4";
+		assertEquals(10,CalculadoraString.add(simples));
+	}
+	
+	@Test
+	public void MinusStringAsDelimitersNoMinusLeft() {
+		String simples = new String();
+		simples = "\\[--][---]\n1-----2--3---4";
+		assertEquals(10,CalculadoraString.add(simples));
+		
+	}
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+	
+	@Test
+	public void MinusStringAsDelimiters1MinusLeft() throws Exception{
+		String simples = new String();
+		expectedEx.expect(NegativeNumbersException.class);
+		expectedEx.expectMessage("negativos proibidos [-2 -3 -5 ]");
+		simples = "\\[-----][------]\n1-------2-------3,-5";
+		assertEquals(11,CalculadoraString.add(simples));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void MinusStringAsDelimiters2MinusLeft() {
+		String simples = new String();
+		simples = "\\[-----][------]\n1--------2,3,4";
+		assertEquals(10,CalculadoraString.add(simples));
+	}
+	
+	
 	
 	
 }
